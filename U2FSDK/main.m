2d621f80fe0bb2cdb4eab406bb212fb687a56ae8
linbaowei <linbaowei_520@sina.com>
2015-11-24 17:32:37 +0800

    Initial Commit

---
+++ U2FSDK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  U2FSDK
+//
+//  Created by 九州云腾 on 15/11/24.
+//  Copyright © 2015年 九州云腾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
